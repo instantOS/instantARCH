8d52873422eb27c4bede3857af1751af64ed87d2
paperbenni <paperbenni@gmail.com>
2020-03-30 00:54:34 +0200

    chmod +x

--- disk/disk.sh
+++ disk/disk.sh
@@ -7,7 +7,7 @@ fdisk -l
 DISK=$(fdisk -l | grep -oi '^Disk /.*:' | fzf --prompt "select disk")
 
 grep -o '/dev/[^:]*' <<<"$DISK" >/root/instantdisk
-echo "selected disk $(cat /root/instantdisk)"
+DISK=$(cat /root/instantdisk)
 
 sed -i "s/instantdisk/$DISK/g" /root/instantARCH/disk/format.sh
 /root/instantARCH/disk/format.sh

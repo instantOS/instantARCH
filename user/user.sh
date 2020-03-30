b6103be00dab217c376a14ccaa68163e3f56d66b
paperbenni <paperbenni@gmail.com>
2020-03-30 16:56:20 +0200

    add user.sh

--- user/user.sh
+++ user/user.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # user and password creation
+source <(curl -Ls https://git.io/JerLG)
+pb dialog
+
+NEWUSER="$(textbox 'set username')"
+NEWPASS="$(passwordbox 'set password')"
+clear
+
+groupadd video
+groupadd wheel
+groupadd docker
+
+useradd -m "$NEWUSER" -s /bin/bash -G wheel,docker,video
+echo "$NEWPASS" | passwd "$NEWUSER" --stdin

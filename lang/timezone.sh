46cb75032869a437b7be6a7c3b6565ee7d10ac36
paperbenni <paperbenni@gmail.com>
2020-03-30 16:19:16 +0200

    add timezone selector

---
+++ lang/timezone.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd /usr/share/zoneinfo
+
+while [ -z "$REGION" ]; do
+    REGION=$(ls | fzf --prompt "select region")
+done
+
+if [ -d "$REGION" ]; then
+    cd "$REGION"
+    while [ -z "$CITY" ]; do
+        City=$(ls | fzf --prompt "select the City nearest to you")
+    done
+fi
+
+if [ -n "$CITY" ]; then
+    ln -sf /usr/share/zoneinfo/$REGION/$CITY /etc/localtime
+    echo "setting timezone to $REGION/$CITY"
+else
+    ln -sf /usr/share/zoneinfo/$REGION /etc/localtime
+    echo "setting timezone to $REGION"
+fi
+
+hwclock --systohc

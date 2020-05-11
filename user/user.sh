bd4473f62101269893cae7f7528e2e60823d7c26
paperbenni <paperbenni@gmail.com>
2020-05-11 17:32:55 +0200

    add autologin

--- user/user.sh
+++ user/user.sh
@@ -12,3 +12,10 @@ groupadd docker &>/dev/null
 useradd -m "$NEWUSER" -s /bin/bash -G wheel,docker,video
 echo "root:$NEWPASS" | chpasswd
 echo "$NEWUSER:$NEWPASS" | chpasswd
+
+groupadd -r autologin
+gpasswd -a "$NEWUSER" autologin
+
+sed -i "s/^#autologin-user=.*/autologin-user=$NEWUSER/" /etc/lightdm/lightdm.conf
+sed -i "s/^#autologin-user-timeout=.*/autologin-user-timeout=0/" /etc/lightdm/lightdm.conf
+sed -i "s/^#autologin-session=.*/autologin-session=instantos/" /etc/lightdm/lightdm.conf

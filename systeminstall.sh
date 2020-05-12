5c2d2dc51b91a5c608fa469b23e1b488165537f6
paperbenni <paperbenni@gmail.com>
2020-05-12 22:47:04 +0200

    add progress reports

--- systeminstall.sh
+++ systeminstall.sh
@@ -10,10 +10,19 @@ serror() {
     exit 1
 }
 
+setinfo() {
+    if [ -e /usr/share/liveutils ]; then
+        pkill instantmenu
+    fi
+    echo "$@" >/opt/instantprogress
+}
+
 escript() {
     rcd
     ./$1.sh || serror
     echo "$1" >>/tmp/instantprogress
+    setinfo "${2:-info}"
+
 }
 
 # scripts executed in installed environment
@@ -21,26 +30,27 @@ chrootscript() {
     rcd
     arch-chroot /mnt "/root/instantARCH/${1}.sh"
     echo "chroot: $1" >>/tmp/instantprogress
+    setinfo "${2:-info}"
 }
 
-chrootscript "depend/depend" &&
-    chrootscript "depend/depend" &&
-    chrootscript "depend/system" &&
-    chrootscript "chroot/chroot" &&
-    chrootscript "chroot/drivers" &&
-    chrootscript "lang/timezone"
+chrootscript "depend/depend" "preparing installer packages" &&
+    chrootscript "depend/depend" "preparing installer packages" &&
+    chrootscript "depend/system" "installing dependencies" &&
+    chrootscript "chroot/chroot" "configuring system" &&
+    chrootscript "chroot/drivers" "installing drivers" &&
+    chrootscript "lang/timezone" "settings time"
 
 # grub: install package, install, generate config
 if efibootmgr; then
-    chrootscript "bootloader/efi"
+    chrootscript "bootloader/efi" "installing bootloader"
 else
-    escript bootloader/install
+    escript bootloader/install "installing bootloader"
 fi
 
-chrootscript "user/user" &&
-    chrootscript "network/network" &&
-    chrootscript "bootloader/config" || exit 1
+chrootscript "user/user" "setting up user" &&
+    chrootscript "network/network" "setting up networkmanager" &&
+    chrootscript "bootloader/config" "configuring bootloader" || exit 1
 
 if ! [ -e /root/instantARCH/config/onlyarch ]; then
-    chrootscript "instantos/install"
+    chrootscript "instantos/install" "configuring instantOS, this will take a while"
 fi

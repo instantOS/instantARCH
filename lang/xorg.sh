12d887bcc17198a96b8d878f16070d270fe05850
paperbenni <paperbenni@gmail.com>
2020-05-15 15:47:21 +0200

    load keymap

--- lang/xorg.sh
+++ lang/xorg.sh
@@ -8,6 +8,7 @@ NEWKEYMAP=$(head -1 /root/instantARCH/data/lang/keyboard/$KEYLANG)
 echo "setting keymap to $NEWXORG"
 
 localectl --no-convert set-x11-keymap "$NEWXORG"
+setxkbmap -layout "$NEWXORG"
 
 if grep -q .. <<<"$NEWKEYMAP"; then
     localectl --no-convert set-keymap "$NEWKEYMAP"

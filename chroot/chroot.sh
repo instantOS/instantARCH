6e1d59e50b76617abb9ab1da44b6f4c8edcdfd25
paperbenni <paperbenni@gmail.com>
2020-05-26 16:49:33 +0200

    typo

--- chroot/chroot.sh
+++ chroot/chroot.sh
@@ -3,9 +3,9 @@
 # enable lightdm greeter
 if grep -q 'greeter-session' /etc/lightdm/lightdm.conf; then
     LASTSESSION="$(grep 'greeter-session' /etc/lightdm/lightdm.conf | tail -1)"
-    sed -i "s/$LASTSESSION/greeter-session=lightdm-gtk-greeter/g" /etc/lightdm.conf
+    sed -i "s/$LASTSESSION/greeter-session=lightdm-gtk-greeter/g" /etc/lightdm/lightdm.conf
 else
-    sed -i 's/^\[Seat:\*\]/\[Seat:\*\]\ngreeter-session=lightdm-gtk-greeter/g' /etc/lightdm.conf
+    sed -i 's/^\[Seat:\*\]/\[Seat:\*\]\ngreeter-session=lightdm-gtk-greeter/g' /etc/lightdm/lightdm.conf
 fi
 
 # fix gui not showing up

c54085f6b11defc449fdf4dffa2089af6c5d6bf7
paperbenni <paperbenni@gmail.com>
2020-06-01 11:50:19 +0200

    add manjaro calamares wrapper

--- manjaro.sh
+++ manjaro.sh
@@ -7,6 +7,25 @@
 
 echo "installing instantOS manjaro edition"
 
+cd /root
+[ -e instantARCH ] && rm -rf instantARCH
+git clone --depth=1 https://github.com/instantos/instantARCH.git
+cd instantARCH
+
+chmod 755 ./*/*.sh
+
+./depend/depend.sh
+./init/init.sh
+
+pacman -S --noconfirm --needed base \
+    linux linux-headers \
+    linux-lts linux-lts-headers \
+    linux-firmware
+
+./depend/system.sh
+./user/modify.sh
+./instantos/install.sh
+
 # not a whole lot of progress made here xD
 
 echo "done installing instantOS"

bf8cccd39df91ea19c5d196bb365e829c68184fa
paperbenni <paperbenni@gmail.com>
2020-06-24 20:34:59 +0200

    add timedatectl

--- lang/timezone.sh
+++ lang/timezone.sh
@@ -12,9 +12,12 @@ fi
 
 if [ -n "$CITY" ]; then
     ln -sf /usr/share/zoneinfo/$REGION/$CITY /etc/localtime
+    timedatectl set-timezone "$REGION/$CITY"
     echo "setting timezone to $REGION/$CITY"
+
 else
     ln -sf /usr/share/zoneinfo/$REGION /etc/localtime
+    timedatectl set-timezone "$REGION"
     echo "setting timezone to $REGION"
 fi
 

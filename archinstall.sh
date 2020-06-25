4b4f3bfa95935b2a92a39f6db15f2f9aa1f0433a
paperbenni <paperbenni@gmail.com>
2020-06-25 17:47:08 +0200

    disable auto reboot on cli

--- archinstall.sh
+++ archinstall.sh
@@ -87,7 +87,8 @@ sudo pkill instantmenu
 sleep 2
 
 # ask to reboot, upload error data if install failed
-if ! [ -e /opt/installfailed ] || ! [ -e /opt/installsuccess ]; then
+if ! [ -e /opt/installfailed ] || ! [ -e /opt/installsuccess ] &&
+    command -v installapplet; then
     notify-send "rebooting"
     sleep 2
     reboot

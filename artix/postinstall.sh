e79a797fedab5521118d156e22e75c4d81026717
paperbenni <paperbenni@gmail.com>
2020-06-26 12:39:31 +0200

    add  artix hooks

---
+++ artix/postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this script only runs on artix
+# and adjusts some stuff that causes problems during installation
+
+if command -v systemctl; then
+    echo "skipping artix hooks"
+    exit
+fi
+
+sed -i '/instantOS trust hook/d' /etc/pacman.conf

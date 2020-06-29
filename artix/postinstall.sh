7cc6f503f476c7ae6eae812ce43c251f553285bd
paperbenni <paperbenni@gmail.com>
2020-06-29 17:20:58 +0200

    fix runit service enabling

--- artix/postinstall.sh
+++ artix/postinstall.sh
@@ -8,9 +8,23 @@ if command -v systemctl; then
     exit
 fi
 
+echo "reverting pacaman fixes"
 sed -i '/Optional TrustAll/d' /etc/pacman.conf
 sleep 1
 
+enableservice() {
+    if [ -e /run/runit/service ]; then
+        echo "enabling runit service $1 in /run/runit/service"
+        ln -s /etc/runit/sv/"$1" /run/runit/service
+    else
+        echo "enabling runit service $1 in /etc/runit/runsvdir/current"
+        ln -s /etc/runit/sv/"$1" /etc/runit/runsvdir/current
+    fi
+}
+/run/runit/service
 # enable services
-ln -s /etc/runit/sv/NetworkManager /run/runit/service
-ln -s /etc/runit/sv/lightdm /run/runit/service
+
+echo "enabling services"
+
+enableservice NetworkManager
+enableservice lightdm

c58c3d57895fdcbc4673e2e1d517ca61d8a41a03
paperbenni <paperbenni@gmail.com>
2020-06-30 17:50:25 +0200

    don't install grub theme on manjaro

--- instantos/install.sh
+++ instantos/install.sh
@@ -51,11 +51,12 @@ bash rootinstall.sh
 [ -e /etc/lightdm ] || mkdir -p /etc/lightdm
 cat /usr/share/instantdotfiles/lightdm-gtk-greeter.conf >/etc/lightdm/lightdm-gtk-greeter.conf
 
-# custom grub theme
-sed -i 's~^#GRUB_THEME.*~GRUB_THEME=/usr/share/grub/themes/instantos/theme.txt~g' /etc/default/grub
-update-grub
-
 # fix grub on manjaro
 if grep -iq 'manjaro' /etc/os-release; then
+    update-grub
     mkinitcpio -P
+else
+    # custom grub theme
+    sed -i 's~^#GRUB_THEME.*~GRUB_THEME=/usr/share/grub/themes/instantos/theme.txt~g' /etc/default/grub
+    update-grub
 fi

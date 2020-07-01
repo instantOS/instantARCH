51183fb2a1278f9a433231639dca13627dbfc0dd
salva09 <salva.hg01@gmail.com>
2020-07-01 20:14:52 +0000

    change variable name to cap

--- pacstrap/pacstrap.sh
+++ pacstrap/pacstrap.sh
@@ -10,25 +10,25 @@ fi
 pacman -Sy --noconfirm
 
 # kernel selection
-kernel="$(iroot kernel)"
+KERNEL="$(iroot kernel)"
 if ! [ kernel -eq "linux" || kernel -eq "linux-lts" ]; then
     # fallbacks to lts kernel
-    kernel="linux-lts"
+    KERNEL="linux-lts"
 fi
 
 # we're on arch
 if command -v pacstrap; then
-    while ! pacstrap /mnt base ${kernel} ${kernel}-headers linux-firmware reflector; do
+    while ! pacstrap /mnt base ${KERNEL} ${KERNEL}-headers linux-firmware reflector; do
         dialog --msgbox "package installation failed \nplease reconnect to internet" 700 700
     done
 else
     # artix or manjaro
     if command -v systemctl; then
-        while ! basestrap /mnt base ${kernel} ${kernel}-headers linux-firmware; do
+        while ! basestrap /mnt base ${KERNEL} ${KERNEL}-headers linux-firmware; do
             dialog --msgbox "manjaro package installation failed \nplease reconnect to internet" 700 700
         done
     else
-        while ! basestrap /mnt runit elogind-runit base base-devel ${kernel} ${kernel}-headers linux-firmware; do
+        while ! basestrap /mnt runit elogind-runit base base-devel ${KERNEL} ${KERNEL}-headers linux-firmware; do
             sleep 2
             dialog --msgbox "artix package installation failed \nplease reconnect to internet" 700 700
         done

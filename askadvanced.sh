cc7c6d38c7fbbff94d5de21c59bcda613d15d5ae
salva09 <salva.hg01@gmail.com>
2020-07-02 23:38:03 +0000

    add option for minimal installation (not yet implemented)

--- askadvanced.sh
+++ askadvanced.sh
@@ -30,10 +30,18 @@ default" | imenu -l "select kernel")
     iroot kernel "$KERNEL"
 }
 
+installation() {
+    TYPE=$(echo "normal
+minimal" | imenu -l "select installation type")
+
+    iroot installation "$TYPE"
+}
+
 while :; do
     CHOICE="$(echo 'autologin
 plymouth
 kernel
+packages
 OK' | imenu -l 'select option')"
     case "$CHOICE" in
     autolog*)
@@ -47,6 +55,9 @@ OK' | imenu -l 'select option')"
 	choosekernel
 	echo "selected $(iroot kernel) kernel"
 	;;
+    packages)
+	installation
+	echo "$(iroot installation) installation"
     OK)
         echo "advanced options done"
         exit

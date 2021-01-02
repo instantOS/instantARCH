ca0b8320c084e05872da212f0a0030e486a3f3a2
paperbenni <paperbenni@gmail.com>
2021-01-02 17:50:05 +0100

    add user shell

---
+++ user/shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# install instantSHELL and plugins
+
+echo "setting up user shell"
+USERNAME="$(iroot user)"
+[ -z "$USERNAME" ] && exit
+[ -e /home/"$USERNAME" ] || exit
+command -v instantshell || exit
+sudo -u "$USERNAME" -c "instantshell install"
+
+echo "finished setting up user shell"

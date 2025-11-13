a099bdfa7789caa817a9cc8416e903aaebc62ad3
paperbenni <paperbenni@gmail.com>
2025-11-13 14:28:20 +0100

    remove non-systemd support

--- lang/apply.sh
+++ lang/apply.sh
@@ -7,12 +7,6 @@ export INSTANTARCH="${INSTANTARCH:-/root/instantARCH}"
 SETLOCALE="$(grep '.' "$INSTANTARCH"/data/lang/locale/"$(iroot locale)" | tail -1 | grep -o '^[^ ]*')"
 
 echo "setting localectl locale to $SETLOCALE"
-if command -v localectl; then
-    localectl set-locale LANG="$SETLOCALE"
-else
-    echo "artix locale configuration"
-    echo 'export LANG="'"$SETLOCALE"'"' >/etc/locale.conf
-    echo 'export LC_COLLATE="C"' >>/etc/locale.conf
-fi
+localectl set-locale LANG="$SETLOCALE"
 
 echo "finished applying locale"
